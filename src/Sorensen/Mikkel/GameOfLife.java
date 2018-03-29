1cd916acace4c9808a87d92509d9180a9289dd64
Mikkel Sørensen <mik4211@edu.easj.dk>
2018-03-29 14:03:00 +0200

    Fået en masser flimmer pixels til at komme frem.

--- src/Sorensen/Mikkel/GameOfLife.java
+++ src/Sorensen/Mikkel/GameOfLife.java
@@ -1,34 +1,81 @@
 package Sorensen.Mikkel;
 
-
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Random;
 
 public class GameOfLife extends Canvas implements Runnable{
 
+    private static final long serialVersionUID = 1L;
+
+    /* Sætter framsize */
     public static int frameSize = 360;
+    /* Sætter title */
     public static String title = "Game Of Life";
 
+    /* Variabler */
+    public BufferedImage image;
+    public int[] pixels;
+    public int gridSize = 100;
+    Random r = new Random();
+
+    /* Constructor */
     public GameOfLife(){
         Dimension d = new Dimension(frameSize, frameSize);
         setMinimumSize(d);
         setMaximumSize(d);
         setPreferredSize(d);
+
+
+        image = new BufferedImage(gridSize, gridSize, BufferedImage.TYPE_INT_RGB);
+        pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
     }
 
-    @Override
-    public void run() {
-        while (true){
 
-        }
-    }
 
     public void start(){
         new Thread(this).start();
     }
 
+    @Override
+    public void run() {
+        while (true){
+            update();
+            render();
+        }
+    }
+
+    public void update() {
+
+    }
+
+    /* Laver en render metode, med en bufferstrategy. Bufferstrategyen hjælper til med at render Canvas / Window */
+    public void render() {
+    BufferStrategy bs = getBufferStrategy();
+
+    if (bs == null){
+        createBufferStrategy(3);
+        return;
+    }
+
+    Graphics g = bs.getDrawGraphics();
+
+
+
+    for (int i = 0; i < pixels.length; i++) {
+        pixels[i] = r.nextInt(0xfffffff);
+    }
+
+    g.drawImage(image, 0, 0, frameSize, frameSize,  null);
+    g.dispose();
+    bs.show();
+    }
     public static void main(String[] args) {
+
+        /* Laver et nyt Jframe, samt definerer hvad der skal ske */
         JFrame frame = new JFrame();
         frame.setTitle(title);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
